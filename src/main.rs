13f4ed8050cf3eea80452f4eba7ca24e04c222a7
Léana 江 <leana.jiang@icloud.com>
2023-05-22 16:49:26 +0200

    major rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,232 +1,70 @@
 use std::fmt;
 
-macro_rules! token_kind {
-    ( $( $kind:ident, $syntax:literal ),+ ) => {
+macro_rules! symbol_type {
+    ( $( $name:ident, $syntax:literal ),+ ) => {
         #[derive(Debug, PartialEq, Eq)]
-        enum TokenKind {
-            Ident(String),
-            $( $kind, )+
-        }
+        enum SymbolType { $( $name, )+ }
 
-        trait Token { fn starts_with_token(&self) -> Option<TokenKind>; }
-        impl Token for &str {
-            fn starts_with_token(&self) -> Option<TokenKind> {
-                match *self {
-                    $( t if t.starts_with($syntax) => Some(TokenKind::$kind), )+
-                    _ => None,
+        fn break_off_token_from_str<'a>(input: &'a str) -> Option<(SymbolType, &'a str)> {
+            use SymbolType::*;
+            $(
+                if let Some(rem) = input.strip_prefix($syntax) {
+                    if rem.starts_with_whitespace() {
+                        return Some(($name, rem));
+                    }
                 }
-            }
+            )+
+
+            return None;
         }
 
-        impl fmt::Display for TokenKind {
+        // Q: remove this?
+        impl fmt::Display for SymbolType {
             fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-                use TokenKind::*;
-                match self {
-                    Ident(s) => write!(f, "{s}"),
-                    $( $kind => write!(f, $syntax), )+
-                }
+                use SymbolType::*;
+                match self { $( $name => write!(f, $syntax), )+ }
             }
         }
     };
 }
 
 #[rustfmt::skip]
-token_kind!(
-    // keywords
-    Read, "read",
-    Write, "write",
-    While, "while",
-    Do, "do",
-    Od, "od",
-    Nil, "nil",
-    Cons, "cons",
-    Hd, "hd",
-    Tl, "tl",
-
-    // symbols
+symbol_type!(
     Percent, "%",
     Assign, ":=",
-    LParen, "(",
-    RParen, ")",
-    Semicolon, ";"
+    Semicolon, ";",
+
+    Read, "read",
+    Write, "write",
+    Do, "do",
+    Od, "od"
 );
 
-#[derive(Default, Debug)]
-struct Tokenizer {
-    input: String,
-    position: usize,
+trait Whitespace {
+    fn starts_with_whitespace(&self) -> bool;
 }
-
-impl Tokenizer {
-    fn new(input: &str) -> Self {
-        Tokenizer {
-            input: input.to_owned(),
-            ..Default::default()
-        }
-    }
-
-    fn skip_whitespace(&mut self) {
-        while let Some(c) = self.peek_char() {
-            if c.is_ascii_whitespace() {
-                self.position += 1;
-            } else {
-                break;
-            }
-        }
-    }
-
-    fn peek_char(&self) -> Option<char> {
-        self.input.chars().nth(self.position)
-    }
-
-    fn remaining_input(&self) -> Option<&str> {
-        if self.position > self.input.len() {
-            None
-        } else {
-            Some(&self.input[self.position..])
-        }
-    }
-
-    fn next_ident(&mut self) -> Option<&str> {
-        let end_position = self
-            .remaining_input()?
-            .find(|c: char| c.is_ascii_whitespace())
-            .map(|p| p + self.position)
-            .unwrap_or(self.input.len());
-
-        // TODO: simplification
-        if self.position < end_position {
-            let s = &self.input[self.position..end_position];
-            Some(s)
-        } else {
-            None
-        }
-    }
-
-    pub fn next_token(&mut self) -> Option<TokenKind> {
-        self.skip_whitespace();
-
-        let token = self
-            .remaining_input()?
-            .starts_with_token()
-            .or_else(|| self.next_ident().map(|s| TokenKind::Ident(s.to_string())));
-
-        match token {
-            Some(ref t) => self.position += t.to_string().len(),
-            None => self.skip_whitespace(),
-        }
-
-        token
+impl Whitespace for &str {
+    fn starts_with_whitespace(&self) -> bool {
+        self.starts_with(' ') || self.starts_with('\n') || self.starts_with('\t') || self.is_empty()
     }
 }
 
-#[test]
-fn next_ident_test_1() {
-    let mut t = Tokenizer::new("foo");
-    assert_eq!(Some("foo"), t.remaining_input());
-    assert_eq!(Some("foo"), t.next_ident());
+enum TokenType {
+    Keyword(SymbolType),
+    Ident(String),
 }
 
 #[test]
-fn next_token_test_1() {
-    let mut t = Tokenizer::new("read");
-    assert_eq!(Some("read"), t.remaining_input());
-    assert_eq!(Some(TokenKind::Read), t.next_token());
-    assert_eq!(None, t.next_token());
+fn break_off_token_from_str_test() {
+    use SymbolType::*;
+    let input = "%";
+    assert_eq!(Some((Percent, "")), break_off_token_from_str(input));
+    let input = ";";
+    assert_eq!(Some((Semicolon, "")), break_off_token_from_str(input));
+    let input = ":=";
+    assert_eq!(Some((Assign, "")), break_off_token_from_str(input));
 }
 
-#[test]
-fn next_token_test_2() {
-    let mut t = Tokenizer::new("%");
-    assert_eq!(Some("%"), t.remaining_input());
-    assert_eq!(Some(TokenKind::Percent), t.next_token());
-    assert_eq!(None, t.next_token());
+fn main() {
+    todo!()
 }
-
-#[test]
-fn next_token_test_3() {
-    let mut t = Tokenizer::new(" ");
-    assert_eq!(Some(" "), t.remaining_input());
-    assert_eq!(None, t.next_token());
-}
-
-#[test]
-fn next_token_test_4() {
-    let mut t = Tokenizer::new("read X\n");
-    assert_eq!(Some(TokenKind::Read), t.next_token());
-    assert_eq!(Some(TokenKind::Ident("X".to_string())), t.next_token());
-    assert_eq!(None, t.next_token());
-}
-
-#[test]
-fn next_token_test_5() {
-    #[rustfmt::skip]
-    let mut t = Tokenizer::new(
-r#"read X
-%
-%
-write Y
-"#,
-    );
-    assert_eq!(Some(TokenKind::Read), t.next_token());
-    assert_eq!(Some(TokenKind::Ident("X".to_string())), t.next_token());
-    assert_eq!(Some(TokenKind::Percent), t.next_token());
-    assert_eq!(Some(TokenKind::Percent), t.next_token());
-    assert_eq!(Some(TokenKind::Write), t.next_token());
-    assert_eq!(Some(TokenKind::Ident("Y".to_string())), t.next_token());
-    assert_eq!(None, t.next_token());
-}
-
-#[test]
-fn next_token_test_6() {
-    use TokenKind::*;
-
-    #[rustfmt::skip]
-    let mut t = Tokenizer::new(
-r#"read X
-%
-    while X do
-        Y := (cons (hd X) Y) ;
-        X := (tl X) ;
-    od
-%
-write Y
-"#,
-    );
-
-    assert_eq!(Some(Read), t.next_token());
-    assert_eq!(Some(Ident("X".to_string())), t.next_token());
-    assert_eq!(Some(Percent), t.next_token());
-
-    assert_eq!(Some(While), t.next_token());
-    assert_eq!(Some(Ident("X".to_string())), t.next_token());
-    assert_eq!(Some(Do), t.next_token());
-
-    assert_eq!(Some(Ident("Y".to_string())), t.next_token());
-    assert_eq!(Some(Assign), t.next_token());
-    assert_eq!(Some(LParen), t.next_token());
-    assert_eq!(Some(Cons), t.next_token());
-    assert_eq!(Some(LParen), t.next_token());
-    assert_eq!(Some(Hd), t.next_token());
-    assert_eq!(Some(Ident("X".to_string())), t.next_token());
-    assert_eq!(Some(RParen), t.next_token());
-    assert_eq!(Some(Ident("Y".to_string())), t.next_token());
-    assert_eq!(Some(RParen), t.next_token());
-    assert_eq!(Some(Semicolon), t.next_token());
-
-    assert_eq!(Some(Ident("X".to_string())), t.next_token());
-    assert_eq!(Some(Assign), t.next_token());
-    assert_eq!(Some(LParen), t.next_token());
-    assert_eq!(Some(Tl), t.next_token());
-    assert_eq!(Some(Ident("X".to_string())), t.next_token());
-    assert_eq!(Some(RParen), t.next_token());
-
-    assert_eq!(Some(Od), t.next_token());
-
-    assert_eq!(Some(Percent), t.next_token());
-    assert_eq!(Some(Write), t.next_token());
-    assert_eq!(Some(Ident("Y".to_string())), t.next_token());
-    assert_eq!(None, t.next_token());
-}
-
-fn main() {}
