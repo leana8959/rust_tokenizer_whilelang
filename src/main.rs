cb7284049220fc64bd9fca9da52aa119dca39fe8
Léana 江 <leana.jiang@icloud.com>
2023-05-23 02:57:15 +0200

    feat: ident working

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ struct Tokenizer {
 }
 
 impl Tokenizer {
-    fn new(input: String) -> Self {
+    fn new(input: &str) -> Self {
         Tokenizer {
-            input,
+            input: input.to_owned(),
             ..Default::default()
         }
     }
@@ -21,17 +21,41 @@ impl Iterator for Tokenizer {
     type Item = TokenType;
 
     fn next(&mut self) -> Option<Self::Item> {
+        use TokenType::*;
         match &self.input[self.position..] {
             "" => None,
-            input => match SymbolType::strip_token(&input) {
-                Some(symbol) => {
-                    self.position += symbol.len();
-                    Some(TokenType::Symbol(symbol))
+            input => match TokenType::strip_symbol(&input)
+                .and_then(|symbol| Some(Symbol(symbol)))
+                .or_else(|| {
+                    TokenType::strip_identifier(&input)
+                        .and_then(|ident| Some(Ident(ident.to_string())))
+                }) {
+                Some(token) => {
+                    self.position += token.len();
+                    Some(token)
                 }
                 None => None,
             },
         }
     }
+}
+
+#[test]
+fn next_token_test_1() {
+    use SymbolType::*;
+    use TokenType::*;
+    let mut t = Tokenizer::new("%;");
+    assert_eq!(Some(Symbol(Percent)), t.next());
+    assert_eq!(Some(Symbol(Semicolon)), t.next());
+    assert_eq!(None, t.next());
+}
+
+#[test]
+fn next_token_test_2() {
+    use TokenType::*;
+    let mut t = Tokenizer::new("X");
+    assert_eq!(Some(Ident("X".to_string())), t.next());
+    assert_eq!(None, t.next());
 }
 
 fn main() {
--- src/token_type.rs
+++ src/token_type.rs
@@ -6,24 +6,27 @@ macro_rules! symbol_type {
         pub enum SymbolType { $( $name, )+ }
 
         impl SymbolType {
-            pub fn strip_token<'a>(input: &'a str) -> Option<SymbolType> {
-                use SymbolType::*;
-                $(
-                    if let Some(rem) = input.strip_prefix($syntax) {
-                        if rem.starts_with_token() {
-                            return Some($name);
-                        }
-                    }
-                )+
-
-                return None;
-            }
-
             pub fn len(&self) -> usize {
                 use SymbolType::*;
                 match self {
                     $( $name => $syntax.len(), )+
                 }
+            }
+        }
+
+        impl TokenType {
+            pub fn strip_symbol<'a>(input: &'a str) -> Option<SymbolType> {
+                use SymbolType::*;
+
+                $(
+                if let Some(rem) = input.strip_prefix($syntax) {
+                    if rem.starts_with_token() {
+                        return Some($name);
+                    }
+                }
+                )+
+
+                return None;
             }
         }
 
@@ -35,6 +38,33 @@ macro_rules! symbol_type {
             }
         }
     };
+}
+
+impl TokenType {
+    pub fn strip_identifier(input: &str) -> Option<&str> {
+        let mut cursor = 0;
+        let mut rem = &input[cursor..];
+
+        while !rem.starts_with_token() {
+            cursor += 1;
+            rem = &input[cursor..];
+        }
+
+        let result = &input[..cursor];
+        if result == "" {
+            None
+        } else {
+            Some(result)
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        use TokenType::*;
+        match self {
+            Symbol(symbol) => symbol.len(),
+            Ident(id) => id.len(),
+        }
+    }
 }
 
 #[rustfmt::skip]
@@ -50,29 +80,36 @@ symbol_type!(
 );
 
 trait Tokenizable {
+    fn starts_with_whitespace(&self) -> bool;
     fn starts_with_token(&self) -> bool;
 }
 impl Tokenizable for &str {
+    fn starts_with_whitespace(&self) -> bool {
+        self.starts_with(' ') || self.starts_with('\n') || self.starts_with('\t')
+    }
     fn starts_with_token(&self) -> bool {
-        self.starts_with(' ')
-            || self.starts_with('\n')
-            || self.starts_with('\t')
-            || self.is_empty()
-            || SymbolType::strip_token(self).is_some()
+        self.starts_with_whitespace() || self.is_empty() || TokenType::strip_symbol(self).is_some()
     }
 }
 
 #[test]
-fn strip_token_test() {
-    use SymbolType::*;
-    let input = "%";
-    assert_eq!(Some(Percent), SymbolType::strip_token(input));
-    let input = ";";
-    assert_eq!(Some(Semicolon), SymbolType::strip_token(input));
-    let input = ":=";
-    assert_eq!(Some(Assign), SymbolType::strip_token(input));
+fn strip_identifier_test() {
+    use TokenType::Ident;
+    assert_eq!(Some("aoeu"), TokenType::strip_identifier("aoeu"));
 }
 
+#[test]
+fn strip_symbol_test() {
+    use SymbolType::*;
+    let input = "%";
+    assert_eq!(Some(Percent), TokenType::strip_symbol(input));
+    let input = ";";
+    assert_eq!(Some(Semicolon), TokenType::strip_symbol(input));
+    let input = ":=";
+    assert_eq!(Some(Assign), TokenType::strip_symbol(input));
+}
+
+#[derive(Debug, PartialEq, Eq)]
 pub enum TokenType {
     Symbol(SymbolType),
     Ident(String),
